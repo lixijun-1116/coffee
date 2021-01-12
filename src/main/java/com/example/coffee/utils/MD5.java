5cd00b1e13e88c119b359f11921d92f0602a12bd
lixijun <1279810365@qq.com>
2021-01-12 10:33:50 +0800

    咖啡-测试修改

--- src/main/java/com/example/coffee/utils/MD5.java
+++ src/main/java/com/example/coffee/utils/MD5.java
@@ -1,6 +1,11 @@
 package com.example.coffee.utils;
 
 import java.security.MessageDigest;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 
 public class MD5 {
 
@@ -59,9 +64,48 @@ public class MD5 {
 //            i++;
 //            foo('d');
 //        }
-        String a = "110**************547";
-        String b = "110108197106295710";
-        System.out.println(a.length());
-        System.out.println(b.length());
+        String a = "140511199811160912";
+        int i = countAge(a);
+        System.out.println(i);
+    }
+
+    //根据身份证号输出年龄
+    public static int countAge(String idNumber) {
+        if(idNumber.length() != 18 && idNumber.length() != 15){
+            throw new IllegalArgumentException("身份证号长度错误");
+        }
+        String year;
+        String yue;
+        String day;
+        if(idNumber.length() == 18){
+            year = idNumber.substring(6).substring(0, 4);// 得到年份
+            yue = idNumber.substring(10).substring(0, 2);// 得到月份
+            day = idNumber.substring(12).substring(0,2);//得到日
+        }else{
+            year = "19" + idNumber.substring(6, 8);// 年份
+            yue = idNumber.substring(8, 10);// 月份
+            day = idNumber.substring(10, 12);//日
+        }
+        Date date = new Date();// 得到当前的系统时间
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        String fyear = format.format(date).substring(0, 4);// 当前年份
+        String fyue = format.format(date).substring(5, 7);// 月份
+        String fday=format.format(date).substring(8,10);//
+        int age = 0;
+        if(Integer.parseInt(yue) == Integer.parseInt(fyue)){//如果月份相同
+            if(Integer.parseInt(day) <= Integer.parseInt(fday)){//说明已经过了生日或者今天是生日
+                age = Integer.parseInt(fyear) - Integer.parseInt(year);
+            }
+        }else{
+            if(Integer.parseInt(yue) < Integer.parseInt(fyue)){
+                //如果当前月份大于出生月份
+                age = Integer.parseInt(fyear) - Integer.parseInt(year);
+            }else{
+                //如果当前月份小于出生月份,说明生日还没过
+                age = Integer.parseInt(fyear) - Integer.parseInt(year) - 1;
+            }
+        }
+        System.out.println("age = " + age);
+        return age;
     }
 }
